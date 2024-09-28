9b4171ee475eb8817c979b86dff16563088f7533
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-09-28 18:01:23 -0300

    feat: add onInit hook to store
    + create loadTaskFromStorage method into store

--- src/app/core/store/index.ts
+++ src/app/core/store/index.ts
@@ -1,7 +1,8 @@
-import { patchState, signalStore, withMethods } from '@ngrx/signals';
+import { patchState, signalStore, withHooks, withMethods } from '@ngrx/signals';
 import { Task, TaskWithoutID } from '@app/core/types';
-import { addEntity, removeEntity, updateEntity, withEntities } from '@ngrx/signals/entities';
+import { addEntities, addEntity, removeEntity, updateEntity, withEntities } from '@ngrx/signals/entities';
 import { generateID } from '@app/core/utils';
+import { effect } from '@angular/core';
 
 export const TasksStore = signalStore(
   { providedIn: 'root' },
@@ -24,5 +25,23 @@ export const TasksStore = signalStore(
     removeTask(id: string): void {
       patchState(store, removeEntity(id));
     },
-  } ))
+    loadTaskFromStorage(): void {
+      try {
+        if ( localStorage.getItem('tasks-store') ) {
+          const tasks: TaskWithoutID[] = JSON.parse(localStorage.getItem('tasks-store')!);
+          patchState(store, addEntities(tasks.map(task => ( { ...task, id: generateID() } ))));
+        }
+      } catch ( _ ) {
+        console.log('La información recuperada del localStorage no tiene un formato correcto');
+      }
+    }
+  } )),
+  withHooks({
+    onInit: (store) => {
+      store.loadTaskFromStorage();
+      effect(() => {
+        localStorage.setItem('tasks-store', JSON.stringify(store.entities()));
+      });
+    }
+  })
 );
